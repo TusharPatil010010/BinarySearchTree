fbf3b9bbb33e3517e5b81c442793bc8602eaeb08
TusharPatil010010 <tusharp010010@gmail.com>
2020-10-16 20:47:37 +0530

    Added functionality to check if all are added using size method

--- src/test/java/com/capg/BinarySearchTree/MyBinaryTreeTest.java
+++ src/test/java/com/capg/BinarySearchTree/MyBinaryTreeTest.java
@@ -14,25 +14,44 @@ class MyBinaryTreeTest {
 
 	@Test
 	void whenOneNodeAdded_shouldReturnOneSize() {
-		MyBinaryTree<Integer> bst = new MyBinaryTree<>();
-		bst.add(56);
-		assertEquals(1, bst.size());
+		MyBinaryTree<Integer> binaryTree = new MyBinaryTree<>();
+		binaryTree.add(56);
+		assertEquals(1, binaryTree.size());
 	}
 
 	@Test
 	void whenTwoNodesAdded_shouldReturnTwoSize() {
-		MyBinaryTree<Integer> bst = new MyBinaryTree<>();
-		bst.add(56);
-		bst.add(30);
-		assertEquals(2, bst.size());
+		MyBinaryTree<Integer> binaryTree = new MyBinaryTree<>();
+		binaryTree.add(56);
+		binaryTree.add(30);
+		assertEquals(2, binaryTree.size());
 	}
 
 	@Test
 	void whenThreeNodesAdded_shouldReturnThreeSize() {
-		MyBinaryTree<Integer> bst = new MyBinaryTree<>();
-		bst.add(56);
-		bst.add(30);
-		bst.add(70);
-		assertEquals(3, bst.size());
+		MyBinaryTree<Integer> binaryTree = new MyBinaryTree<>();
+		binaryTree.add(56);
+		binaryTree.add(30);
+		binaryTree.add(70);
+		assertEquals(3, binaryTree.size());
+	}
+	
+	@Test
+	void whenAllNodesAdded_shouldReturn_itsSize() {
+		MyBinaryTree<Integer> bstbinaryTree = new MyBinaryTree<>();
+		bstbinaryTree.add(56);
+		bstbinaryTree.add(30);
+		bstbinaryTree.add(70);
+		bstbinaryTree.add(22);
+		bstbinaryTree.add(40);
+		bstbinaryTree.add(11);
+		bstbinaryTree.add(3);
+		bstbinaryTree.add(16);
+		bstbinaryTree.add(60);
+		bstbinaryTree.add(95);
+		bstbinaryTree.add(65);
+		bstbinaryTree.add(63);
+		bstbinaryTree.add(67);
+		assertEquals(13, bstbinaryTree.size());
 	}
 }
